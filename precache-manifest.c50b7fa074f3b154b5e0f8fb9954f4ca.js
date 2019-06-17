af7ab1f254e0726177ce7dc7823ff257cc7b7784
Sugih Ilmi Kalih Putra <sugih.ikp@gmail.com>
2019-06-17 13:44:14 +0700

    Updates

---
+++ precache-manifest.c50b7fa074f3b154b5e0f8fb9954f4ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c1e40b1f22b8102438ec51a301bdc9",
+    "url": "/marioplan/index.html"
+  },
+  {
+    "revision": "ae55f6b40d1c068f169e",
+    "url": "/marioplan/static/css/main.2344c22e.chunk.css"
+  },
+  {
+    "revision": "c3e37dd08f0d70fe0f9d",
+    "url": "/marioplan/static/js/2.b34480a7.chunk.js"
+  },
+  {
+    "revision": "ae55f6b40d1c068f169e",
+    "url": "/marioplan/static/js/main.17813ff7.chunk.js"
+  },
+  {
+    "revision": "cba2007afcbac50b77fd",
+    "url": "/marioplan/static/js/runtime~main.c1b9aef5.js"
+  }
+]);
